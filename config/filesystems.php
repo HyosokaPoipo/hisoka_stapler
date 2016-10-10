8d1abebcbd90e251e9e0a5f589196823d5553149
Maryadi <maryadi@astrnt.co>
2016-10-10 15:08:33 +0700

    nambain s3_client_config

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,6 +62,14 @@ return [
             'bucket' => env('S3_BUCKET'),
         ],
 
+        's3_client_config' => [
+            'driver' => 's3',
+            'key'    => env('S3_KEY'),
+            'secret' => env('S3_SECRET'),
+            'region' => env('S3_REGION'),
+            'bucket' => env('S3_BUCKET'),
+        ],
+
     ],
 
 ];
