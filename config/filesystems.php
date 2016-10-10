031b6a6ca748ba3965f2c3e3875efb31d278af1f
Maryadi <maryadi@astrnt.co>
2016-10-10 15:40:00 +0700

    add new config again...

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,6 +66,7 @@ return [
         'key' => env('S3_KEY'),
         'secret' => env('S3_SECRET'),
         'region' => env('S3_REGION'),
+        'bucket' => env('S3_BUCKET'),
         'scheme' => 'http',
         ],
 
